bd189bbd09cb7c01ad936b2ad9de79fa9588d4c2
r11Sr <rs_burner2@outlook.com>
2021-10-08 18:46:21 -0500

    Exercise 2 - Add an X or O to a square when clicked

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,21 +1,58 @@
 
-
 document.addEventListener("DOMContentLoaded", function(event) {
     let allsquares = document.getElementById('board');
     let children = allsquares.childNodes;
-    // console.log(allsquares.childNodes);
 
-
+    // Exercise 1
+    
     [...children].forEach((e) =>{e.className = 'square';});
-    // children.forEach( function (obj){
-    //     this.className = 'square';
-    // });
+
+
+    // Exercise 2
+    let choice = 0;
+
+
+    const boardArray = Array.from(document.querySelectorAll('.square'));
+
+    const conbinationsToWin = [
+        [2,5,8],
+        [1,4,7],
+        [0,3,6],
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,4,8],
+        [2,4,6]
+    ];
+    
+    [...children].forEach((e) => 
+    {e.addEventListener("click",function (e){
+                if (choice%2 == 0)
+                {
+                   
+                    e.target.textContent = 'X';
+                    e.target.className += ' X';
+                    choice++;
+                }
+                else 
+                {
+                    e.target.textContent = 'O';
+                    e.target.className += ' O';
+                    choice++;
+                }
+            }
+    
+        );
+    
+      }
+    
+    );
+    
+
+
 });
 
 
-// window.onload = function(){
-//     let e = document.getElementById('#board');
-//     console.log(e);
-// }
+
 
 
